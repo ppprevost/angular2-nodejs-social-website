7fbd5a6717a38c9a434c2069a6222258fa2bc223
Prevost <ppprevost@aol.fr>
2017-06-07 23:03:14 +0200

    add interface in the resolve

--- src/app/follow-user/follow-user-resolver.service.ts
+++ src/app/follow-user/follow-user-resolver.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
+import {User} from '../interface/interface';
 import {
   Router, Resolve, RouterStateSnapshot,
   ActivatedRouteSnapshot
@@ -8,17 +9,13 @@ import {DataService} from '../services/data.service';
 
 
 @Injectable()
-export class FollowUserResolverService implements Resolve<any> {
+export class FollowUserResolverService implements Resolve<User[]> {
   constructor(private data: DataService, private router: Router) {
 
   }
 
-  resolve(route: ActivatedRouteSnapshot): Promise<any> {
-    return this.data.getUsers()
-    //   .then(users => {
-    //   if (users) {
-    //     return users;
-    //   }
-    // });
+  resolve(route: ActivatedRouteSnapshot): Promise<User[]> {
+    return this.data.getUsers();
+
   }
 }
