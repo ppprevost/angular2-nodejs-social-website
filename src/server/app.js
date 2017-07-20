3e7fa685c674b4ad01da4f6c4379f827df63fdf3
Pierre-Philippe <pierre-philippe.prevost.prestataire@sfr.com>
2017-07-20 12:37:26 +0200

    add env file

--- src/server/app.js
+++ src/server/app.js
@@ -24,7 +24,7 @@ limiter({
 });
 
 let dirEnv = path.join(process.cwd(), '/.env');
-let contentEnv = "MONGODB_URI=mongodb://localhost:27017/test \nMAILVERIF=Gmail \nURLVERIF=http://example.com/email-verification/${URL} \nMAILACCOUNT= \nMAILPASS= \nCLIENTID= \nACCESSTOKEN= \nREFRESHTOKEN= \nCLIENTSECRET= \nSECRET_TOKEN=social\nSECRET_KEYCAPTCHA= ";
+let contentEnv = "MONGODB_URI=mongodb://localhost:27017/test \nMAILVERIF=Gmail \nURLVERIF=http://example.com/email-verification/${URL} \nMAILACCOUNT= \nMAILPASS= \nCLIENTID= \nACCESSTOKEN= \nREFRESHTOKEN= \nCLIENTSECRET= \nSECRET_TOKEN=social\nSECRET_KEYCAPTCHA= \nRECAPTCHA_MODULE=false \nEMAIL_VERIFICATION=false";
 try {
   fs.statSync(dirEnv).isFile()
 } catch (err) {
