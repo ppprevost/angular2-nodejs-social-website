5cdf16f5ff33547068ee5e375d31403d6a69a8e6
Pierre-Philippe <pierre-philippe.prevost.prestataire@sfr.com>
2017-08-21 11:10:11 +0200

    maj readme and fix env variable 2

--- server/app.ts
+++ server/app.ts
@@ -61,22 +61,6 @@ class Server {
       });
     });
   }
-
-  environment() {
-    const dirEnv = path.join(process.cwd(), '/.env');
-    dotenv.config({path: '.env'});
-    const contentEnv = 'MONGODB_URI=mongodb://localhost:27017/test \nMAILVERIF=Gmail \nURLVERIF=http://example.com/email-verification/${URL} \nMAILACCOUNT= \nMAILPASS= \nCLIENTID= \nACCESSTOKEN= \nREFRESHTOKEN= \nCLIENTSECRET= \nSECRET_TOKEN=social\nSECRET_KEYCAPTCHA= \nRECAPTCHA_MODULE=false \nEMAIL_VERIFICATION=false';
-    try {
-      fs.statSync(dirEnv).isFile();
-    } catch (err) {
-      if (err.code === 'ENOENT') {
-        if (!process.env.MONGODB_URI) {
-          console.log('environment file does not exist, please fulfill the information in the dot env file at the root folder');
-          fs.writeFileSync(dirEnv, contentEnv, 'utf8');
-        }
-      }
-    }
-  }
 }
 
 export default new Server();
